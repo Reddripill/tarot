ff669bf66425c249592dafbf37305288e3727362
Reddripill <reddripillll@gmail.com>
2025-02-24 20:48:10 +0300

    fix tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -26,7 +26,7 @@ export default {
             header:
                "rgba(0, 0, 0, 0.07) 0px 1px 2px, rgba(0, 0, 0, 0.07) 0px 2px 4px, rgba(0, 0, 0, 0.07) 0px 4px 8px, rgba(0, 0, 0, 0.07) 0px 8px 16px, rgba(0, 0, 0, 0.07) 0px 16px 32px, rgba(0, 0, 0, 0.07) 0px 32px 64px",
             button:
-               "rgba(255, 223, 175, 0.15) 0px 15px 25px, rgba(255, 223, 175, 0.05) 0px 5px 10px;",
+               "rgba(255, 223, 175, 0.15) 0px 15px 25px, rgba(255, 223, 175, 0.05) 0px 5px 10px",
          },
          transitionDuration: {
             DEFAULT: "300ms",
